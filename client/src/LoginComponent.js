a894cef98a9f6287b6262f76d610f0efbedfae0a
Quocdat1205 <quocdatsc1205@gmail.com>
2022-06-20 09:19:29 +0700

    init

--- client/src/LoginComponent.js
+++ client/src/LoginComponent.js
@@ -1,42 +1,56 @@
 import React, { Component } from "react";
 import { getMessage } from "../../common/consts";
+import axios from "axios";
 
 class LoginComponent extends Component {
   constructor(props) {
     super(props);
     this.authenticate = this.authenticate.bind(this);
-    this.state = { access_token: "" };
+    this.state = { accessToken: "" };
   }
 
   authenticate = async () => {
-    const accountAddress = this.props.account;
+    const walletAddress = this.props.account;
     const signature = await this.props.web3.currentProvider.send(
       "personal_sign",
       [
         getMessage(),
-        accountAddress, //from which account should be signed. Web3, metamask will sign message by private key inconspicuously.
+        walletAddress, // from which account should be signed. Web3, metamask will sign message by private key inconspicuously.
       ]
     );
     const signatureResult = signature.result;
-    const requestOptions = {
-      method: "POST",
-      headers: { "Content-Type": "application/json" },
-      body: JSON.stringify({ accountAddress, "signature":signatureResult }),
-    };
-    //make request to local server
-    return fetch(`http://localhost:3001/auth/login`, requestOptions)
-      .then((response) => response.json())
-      .then((token) => {
-        this.setState({ access_token: token });
-      });
+
+    try {
+      // make request to local server
+      const { data } = await axios.post(
+        `http://localhost:4000/api/insurance/user/log-in`,
+        { walletAddress, signature: signatureResult },
+        { withCredentials: true }
+      );
+
+      this.setState({ accessToken: data });
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  handlerCheck = async () => {
+    const request = await axios.get(
+      `http://localhost:4000/api/insurance/user/check-expire`,
+      { withCredentials: true }
+    );
+    console.log(request);
   };
 
   render() {
     return (
       <div>
         <br></br>
-        {this.state.access_token ? (
-          `User access token: ${this.state.access_token}`
+        {this.state.accessToken ? (
+          <>
+            `User access token: ${this.state.accessToken}`
+            <button onClick={() => this.handlerCheck()}>Click</button>
+          </>
         ) : (
           <button onClick={this.authenticate}>Login</button>
         )}
